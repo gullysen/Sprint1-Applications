14bc08ac4054db0ff176ff89a3f696db15e721a4
marydouETH <johnniejia7@gmail.com>
2025-02-26 18:08:50 +0900

    feat: complete full contract

---
+++ marydouETH/cli/command.ts
@@ -0,0 +1,416 @@
+import { program } from 'commander';
+import { PublicKey } from '@solana/web3.js';
+import {
+  changeAdmin,
+  getGlobalInfo,
+  initProject,
+  setClusterConfig,
+  changeConfig,
+  createIco,
+  closeIco,
+  buyToken,
+  claim,
+  withdrawCost,
+  rescueToken,
+  getIcoInfo,
+  getAllICOs,
+  getUserPurchaseInfo,
+  getAllPurchases,
+} from './scripts';
+
+// program.version('0.0.1');
+
+programCommand('status')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+    await setClusterConfig(env, keypair, rpc);
+
+    console.log(await getGlobalInfo());
+  });
+
+programCommand('init')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await initProject();
+  });
+
+programCommand('change-admin')
+  .requiredOption('-a, --new_admin <string>', 'new admin address')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, new_admin } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+    await setClusterConfig(env, keypair, rpc);
+
+    //  update global info
+    await changeAdmin(new_admin);
+  });
+
+programCommand('change-config')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-p, --paused <boolean>')
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, paused } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await changeConfig(paused === 'true');
+  });
+
+programCommand('create-ico')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-i --ico_mint <string>')
+  .option(
+    '-ip --ico_is_token22 <boolean>',
+    'should true if ico mint is token 2022'
+  )
+  .requiredOption('-c --cost_mint <string>')
+  .option(
+    '-cp --cost_is_token22 <boolean>',
+    'should true if cost mint is token 2022'
+  )
+  .requiredOption('-m --amount <number>', 'ico token amount to sell')
+  .requiredOption(
+    '-stp --start_price <number>',
+    'price of 1 token in cost tokens'
+  )
+  .requiredOption(
+    '-enp --end_price <number>',
+    'if 0 then price is fixed, else price grows liner from startPrice to endPrice based on sold tokens'
+  )
+  .requiredOption(
+    '-std --start_date <number>',
+    'timestamp when ICO starts. The date must be in future'
+  )
+  .requiredOption(
+    '-end --end_date <number>',
+    'timestamp when ICO ends, if 0 then ICO will be active until sell all tokens'
+  )
+  .requiredOption(
+    '-br --bonus_reserve <number>',
+    'amount of tokens that will be used for bonus'
+  )
+  .requiredOption(
+    '-bp --bonus_percentage <number>',
+    'percent of bonus (with 2 decimals) which will be added to bought amount'
+  )
+  .requiredOption(
+    '-ba --bonus_activator <number>',
+    'percent of total ICO tokens that should be bought to activate bonus (with 2 decimals)'
+  )
+  .requiredOption(
+    '-up --unlock_percentage <number>',
+    'percentage (with 2 decimals) of initially unlocked token'
+  )
+  .requiredOption('-cfp --cliff_period <number>', 'cliff period (in seconds)')
+  .requiredOption(
+    '-vp --vesting_percentage <number>',
+    'percentage (with 2 decimals) of locked tokens will be unlocked every interval'
+  )
+  .requiredOption(
+    '-vi --vesting_interval <number>',
+    'interval (in seconds) of vesting'
+  )
+  .action(async (directory, cmd) => {
+    const {
+      env,
+      keypair,
+      rpc,
+      ico_mint,
+      cost_mint,
+      ico_is_token22,
+      cost_is_token22,
+      amount,
+      start_price,
+      end_price,
+      start_date,
+      end_date,
+      bonus_reserve,
+      bonus_percentage,
+      bonus_activator,
+      unlock_percentage,
+      cliff_period,
+      vesting_percentage,
+      vesting_interval,
+    } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+    await setClusterConfig(env, keypair, rpc);
+
+    await createIco(
+      new PublicKey(ico_mint),
+      new PublicKey(cost_mint),
+      {
+        amount: amount as string,
+        startPrice: start_price as string,
+        endPrice: end_price as string,
+        startDate: Number(start_date),
+        endDate: Number(end_date),
+
+        bonusReserve: bonus_reserve as string,
+        bonusPercentage: Number(bonus_percentage),
+        bonusActivator: Number(bonus_activator),
+
+        unlockPercentage: Number(unlock_percentage),
+        cliffPeriod: Number(cliff_period),
+        vestingPercentage: Number(vesting_percentage),
+        vestingInterval: Number(vesting_interval),
+      },
+      !ico_is_token22 ? undefined : true,
+      !cost_is_token22 ? undefined : true
+    );
+  });
+
+programCommand('close-ico')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-i, --ico <string>', 'ico PDA address')
+  .option(
+    '-ip --ico_is_token22 <boolean>',
+    'should true if ico mint is token 2022'
+  )
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, ico, ico_is_token22 } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await closeIco(new PublicKey(ico), !ico_is_token22 ? undefined : true);
+  });
+
+programCommand('buy-token')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-i, --ico <string>')
+  .requiredOption('-a, --amount <number>', 'amount to buy ico tokens')
+  .option(
+    '-ip --ico_is_token22 <boolean>',
+    'should true if ico mint is token 2022'
+  )
+  .option(
+    '-cp --cost_is_token22 <boolean>',
+    'should true if cost mint is token 2022'
+  )
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, ico, amount, ico_is_token22, cost_is_token22 } =
+      cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await buyToken(
+      new PublicKey(ico),
+      amount as string,
+      !ico_is_token22 ? undefined : true,
+      !cost_is_token22 ? undefined : true
+    );
+  });
+
+programCommand('claim')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-i, --ico <string>')
+  .requiredOption('-u, --user_purchase <string>')
+  .option(
+    '-ip --ico_is_token22 <boolean>',
+    'should true if ico mint is token 2022'
+  )
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, ico, user_purchase, ico_is_token22 } =
+      cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await claim(
+      new PublicKey(ico),
+      new PublicKey(user_purchase),
+      !ico_is_token22 ? undefined : true
+    );
+  });
+
+programCommand('withdraw-cost')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-i, --ico <string>')
+  .option(
+    '-cp --cost_is_token22 <boolean>',
+    'should true if cost mint is token 2022'
+  )
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, ico, cost_is_token22 } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await withdrawCost(new PublicKey(ico), !cost_is_token22 ? undefined : true);
+  });
+
+programCommand('rescue-token')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .requiredOption('-i, --ico <string>')
+  .option(
+    '-ip --ico_is_token22 <boolean>',
+    'should true if ico mint is token 2022'
+  )
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, ico, ico_is_token22 } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    await rescueToken(new PublicKey(ico), !ico_is_token22 ? undefined : true);
+  });
+
+programCommand('get-ico')
+  .requiredOption('-i, --ico <string>')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, ico } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    console.log(await getIcoInfo(new PublicKey(ico)));
+  });
+
+programCommand('all-icos')
+  .option('-o, --owner <string>', 'filter by ICO pot owner')
+  .option('-i, --ico_mint <string>', 'filter by ICO token mint')
+  .option('-c, --cost_mint <string>', 'filter by cost token mint')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, owner, ico_mint, cost_mint } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    console.dir(
+      await getAllICOs({
+        owner: owner ? new PublicKey(owner) : undefined,
+        icoMint: ico_mint ? new PublicKey(ico_mint) : undefined,
+        costMint: cost_mint ? new PublicKey(cost_mint) : undefined,
+      }),
+      {
+        depth: null,
+      }
+    );
+  });
+
+programCommand('user-purchase')
+  .requiredOption('-a, --user_purchase <string>')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, market, user_purchase } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    console.log(await getUserPurchaseInfo(new PublicKey(user_purchase)));
+  });
+
+programCommand('all-purchases')
+  .option('-b, --buyer <string>', 'filter by buyer address')
+  .option('-i, --ico <string>', 'filter by ico pot address')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (directory, cmd) => {
+    const { env, keypair, rpc, buyer, ico } = cmd.opts();
+
+    console.log('Solana Cluster:', env);
+    console.log('Keypair Path:', keypair);
+    console.log('RPC URL:', rpc);
+
+    await setClusterConfig(env, keypair, rpc);
+
+    console.dir(
+      await getAllPurchases({
+        buyer: buyer ? new PublicKey(buyer) : undefined,
+        ico: ico ? new PublicKey(ico) : undefined,
+      }),
+      {
+        depth: null,
+      }
+    );
+  });
+
+function programCommand(name: string) {
+  return (
+    program
+      .command(name)
+      .option('-e, --env <string>', 'Solana cluster env name', 'devnet') //mainnet-beta, testnet, devnet
+      .option(
+        '-r, --rpc <string>',
+        'Solana cluster RPC name'
+        // 'https://devnet.helius-rpc.com/?api-key=44b7171f-7de7-4e68-9d08-eff1ef7529bd'
+        // 'https://flashy-cosmological-pallet.solana-mainnet.quiknode.pro/86e2f4bc350a6fb8dbcb110df4d030a205455f70/'
+      )
+      // .option('-r, --rpc <string>', 'Solana cluster RPC name', 'https://mainnet.helius-rpc.com/?api-key=99c6d984-537e-4569-955b-5e4703b73c0d')
+      .option(
+        '-k, --keypair <string>',
+        'Solana wallet Keypair Path',
+        '../deploy.json'
+      )
+  );
+}
+
+program.parse(process.argv);
+
+/*
+
+yarn script init
+yarn script change-admin -n J9ja5QkewwMi9kG6JkCNxfLK9CoDGk3F4hZTNKQaKZe3
+yarn script lock -m BV3bvkBqVawTghH4uCaba3MGgYs63XyxwX9CeULwvmKG
+
+https://solana-mainnet.g.alchemy.com/v2/wsOJ8IVuGPfyljRfcZjpLrsVQu0_of-j
+
+yarn script unlock -m AXXfo3sggcMLNvz3zRS2wJz8xy78DFbxmgcsUYkM5TzQ -k ../key/G2.json
+
+yarn script user-status -a G2sc5mU3eLRkbRupnupzB3NTzZ85bnc9L1ReAre9dzFU
+yarn script user-status -a 4EjZ4sGnvfLbW89AAzSehob7Rmkym7vCH3SMcThSx9q1
+
+yarn script get-users
+yarn script status
+
+*/
--- marydouETH/programs/ico-launchpad/src/state/ico_states.rs
+++ marydouETH/programs/ico-launchpad/src/state/ico_states.rs
@@ -2,12 +2,179 @@ use anchor_lang::prelude::*;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy)]
 pub struct CreateIcoParams {
+    pub amount: u64,
+    pub start_price: u64,
+    pub end_price: u64,
+    pub start_date: i64,
+    pub end_date: i64,
+
+    pub bonus_reserve: u64,
+    pub bonus_percentage: u16,
+    pub bonus_activator: u16,
+
+    pub unlock_percentage: u16,
+    pub cliff_period: i64,
+    pub vesting_percentage: u16,
+    pub vesting_interval: i64,
 }
 
 #[account]
 #[derive(Default)]
 pub struct IcoState {
+    pub seed: u64,     // id of ICO
+    pub owner: Pubkey, //  address of ICO owner (creator) who will receive payment tokens
+
+    pub ico_mint: Pubkey,  // ICO token
+    pub ico_decimals: u64, // decimal of ICO  - 10 ^ 9 default
+    pub amount: u64,       // amount of token to sell
+
+    pub cost_mint: Pubkey, // if address(0) - native coin
+    pub start_price: u64,  // price of 1 token in cost tokens
+    pub end_price: u64, // if 0 then price is fixed, else price grows liner from startPrice to endPrice based on sold tokens.
+    pub start_date: i64, // timestamp when ICO starts. The date must be in future.
+    pub end_date: i64, // timestamp when ICO ends, if 0 then ICO will be active until sell all tokens
+
+    pub bonus_reserve: u64, // amount of tokens that will be used for bonus. Bonus will be paid until it's available
+    pub bonus_percentage: u16, // percent of bonus (with 2 decimals) which will be added to bought amount. I.e. 2500 = 25%
+    pub bonus_activator: u16, // percent of total ICO tokens that should be bought to activate bonus (with 2 decimals). I.e. 1000 = 10%
+    // Let say total amount of tokens on this ICO is 1,000,000, so to receive bonus a user should buy at least 100,000 tokens at ones (10%)
+    pub is_closed: u8, // ICO is closed
+
+    pub total_sold: u64,     // total amount of sold tokens
+    pub total_received: u64, // total amount received (in paymentToken)
+
+    // parameters of vesting
+    pub unlock_percentage: u16, // percentage (with 2 decimals) of initially unlocked token. I.e. 500 means 5% unlocked and 95% will go to vesting, 10000 means 100% unlocked (vesting will not be used)
+    pub cliff_period: i64, // cliff period (in seconds). The first release will be after this time.
+    pub vesting_percentage: u16, // percentage (with 2 decimals) of locked tokens will be unlocked every interval I.e. 500 means 5% per vestingInterval
+    pub vesting_interval: i64,   // interval (in seconds) of vesting (i.e. 2592000 = 30 days)
+
+    pub purchase_seq_num: u64, // seq num to identify users purchases
+
+    pub extra: u128,
 }
 
 impl IcoState {
-    pub const DATA_SIZE: usize = 8 + std::mem::size_of::<IcoState>();}
+    pub const DATA_SIZE: usize = 8 + std::mem::size_of::<IcoState>();
+
+    // return value of cost token to pay for the ico tokens amount
+    pub fn get_value(&self, amount: u64) -> (u64, u64) {
+        let mut available_amount = self.amount - self.total_sold;
+
+        if amount < available_amount {
+            available_amount = amount;
+        }
+
+        if self.end_price == 0 {
+            // fixed price
+            let value = available_amount
+                .checked_mul(self.start_price)
+                .unwrap()
+                .checked_div(self.ico_decimals)
+                .unwrap();
+            return (available_amount, value);
+        } else {
+            let current_price = self.start_price
+                + (self.end_price - self.start_price)
+                    .checked_mul(self.total_sold)
+                    .unwrap()
+                    .checked_div(self.amount)
+                    .unwrap();
+            let executed_end_price = self.start_price
+                + (self.end_price - self.start_price)
+                    .checked_mul(available_amount)
+                    .unwrap()
+                    .checked_div(self.amount)
+                    .unwrap();
+            let value = available_amount
+                .checked_mul(current_price + executed_end_price)
+                .unwrap()
+                .checked_div(2 * self.ico_decimals)
+                .unwrap();
+            return (available_amount, value);
+        }
+    }
+
+    pub fn get_bonus(&mut self, amount: u64) -> u64 {
+        if self.bonus_reserve == 0 {
+            return 0;
+        }
+
+        let bonus_condition = self
+            .amount
+            .checked_mul(self.bonus_activator as u64)
+            .unwrap()
+            / 10000;
+
+        if bonus_condition <= amount {
+            let mut bonus = amount.checked_mul(self.bonus_percentage as u64).unwrap() / 10000;
+            if bonus > self.bonus_reserve {
+                bonus = self.bonus_reserve;
+            }
+            return bonus;
+        }
+
+        return 0;
+    }
+
+    pub fn get_unlocked(&self, amount: u64) -> (u64, u64) {
+        let unlocked = amount.checked_mul(self.unlock_percentage as u64).unwrap() / 10000;
+        (unlocked, amount - unlocked)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ico_calc() -> Result<()> {
+        let mut mockup: IcoState = IcoState {
+            seed: 0,
+            owner: Pubkey::default(),
+
+            ico_mint: Pubkey::default(),
+            ico_decimals: 1_000_000_000,   // decimal is 9
+            amount: 1_000_000_000_000_000, // 1M tokens
+
+            cost_mint: Pubkey::default(),
+            start_price: 1_000_000_000, // 1 cost token for 1 ICO token
+            end_price: 0,               // use fixed price
+            start_date: 10000,          // current timestamp mockup
+            end_date: 0,                // selling until all tokens are sold
+
+            bonus_reserve: 1_000_000_000_000, // use 1k tokens as bonus
+            bonus_percentage: 10,             // 0.1% of total bonus reserve
+            bonus_activator: 100,             // 1% of total ICO tokens amount
+
+            is_closed: 0, // 1 is closed
+
+            total_sold: 0,
+            total_received: 0,
+
+            unlock_percentage: 10000, // not use vesting
+            cliff_period: 0,
+            vesting_percentage: 0,
+            vesting_interval: 0,
+
+            purchase_seq_num: 0, // increase sequantly every purchase
+
+            extra: 0, // extra space for version upgrade
+        };
+
+        let test_amount = 1_000_000_000;
+        let big_test_amount = 100_000_000_000_000;
+        let (available, cost_value) = mockup.get_value(test_amount);
+        assert_eq!(available, test_amount);
+        assert_eq!(cost_value, test_amount);
+
+        let bonus = mockup.get_bonus(big_test_amount);
+        assert_eq!(bonus, 100_000_000_000);
+
+        let (unlocked, locked) = mockup.get_unlocked(big_test_amount);
+        assert_eq!(unlocked, big_test_amount);
+        assert_eq!(locked, 0);
+
+        Ok(())
+    }
+}
